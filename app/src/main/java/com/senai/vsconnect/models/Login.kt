4bf07866db911b8e9c72482ab39859667426de8e
Aléxia Vitória Melhado Pereira <aleeevitoria@gmail.com>
2024-01-05 16:13:42 -0300

    subindo correções na estrutura do login

--- app/src/main/java/com/senai/vsconnect/models/Login.kt
+++ app/src/main/java/com/senai/vsconnect/models/Login.kt
@@ -2,4 +2,4 @@ package com.senai.vsconnect.models
 
 import java.util.*
 
-class Login(email: String, password: String) {}
+data class Login(val email: String, val password: String) {}
--- app/src/main/java/com/senai/vsconnect/views/LoginFragment.kt
+++ app/src/main/java/com/senai/vsconnect/views/LoginFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.google.gson.JsonObject
@@ -19,7 +21,7 @@ import retrofit2.Response
 
 class LoginFragment : Fragment() {
 
-    private val retrofitClient = NetworkUtils.getRetrofitInstance("http://IP:8099/")
+    private val retrofitClient = NetworkUtils.getRetrofitInstance("http://192.168.1.102:8099/")
     private val endpointFile = retrofitClient.create(Endpoint::class.java)
     private var _binding: FragmentLoginBinding? = null
     private val binding get() = _binding!!
@@ -35,18 +37,42 @@ class LoginFragment : Fragment() {
 
         val btnEntrar = root.findViewById<Button>(R.id.btn_entrar)
 
+
+
         btnEntrar.setOnClickListener {
-            // Navegar para a tela de serviços ao clicar no botão
-            findNavController().navigate(R.id.action_nav_sair_to_nav_servicos)
+            val idEmail = root.findViewById<EditText>(R.id.campo_email)
+            val idSenha = root.findViewById<EditText>(R.id.campo_senha)
+
+            // Para obter o texto digitado
+            val emailDigitado = idEmail.text.toString()
+            val senhaDigitada = idSenha.text.toString()
+
+            val user = Login(email = emailDigitado, password = senhaDigitada)
+
+            val usuarioLogado = logado(user);
+
+            if (usuarioLogado){
+                // Navegar para a tela de serviços ao clicar no botão
+                findNavController().navigate(R.id.nav_servicos)
+            }else{
+                Toast.makeText(requireContext(), "Usuário não encontrado!", Toast.LENGTH_SHORT).show()
+            }
         }
 
-        val user = Login(email = "thiago3@email.com", password = "senai")
+        return root
+    }
+
+    fun logado(user: Login): Boolean{
+
+        var retornoAPI: Boolean = false
 
         // Fazendo uma requisição de login
         endpointFile.login(user).enqueue(object : Callback<JsonObject> {
             override fun onFailure(call: retrofit2.Call<JsonObject>, t: Throwable) {
                 // Tratamento de falha ao fazer a requisição
                 println("Falha na requisição de login: ${t.message}")
+                retornoAPI = false
+
             }
 
             override fun onResponse(call: retrofit2.Call<JsonObject>, response: Response<JsonObject>) {
@@ -55,17 +81,20 @@ class LoginFragment : Fragment() {
                     val jsonObject = response.body()
                     // Processar o objeto JSON
                     println(jsonObject)
+
+                    retornoAPI = true
                 } else {
                     // Tratamento de erro na resposta
                     println("Erro na resposta de login: ${response.code()}")
+                    println("Corpo da resposta: ${response.errorBody()?.string()}")
+                    retornoAPI = false
                 }
             }
 
         })
 
-        return root
+        return retornoAPI
     }
-
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
