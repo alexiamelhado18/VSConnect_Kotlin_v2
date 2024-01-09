ee625b91314cf685d9d1b10cd8265ff6d89adebf
unknown <Alexia@DESKTOP-AP18MAI.info.local>
2024-01-09 16:44:49 -0300

    refatoração

--- app/src/main/java/com/senai/vsconnect/apis/Endpoint.kt
+++ app/src/main/java/com/senai/vsconnect/apis/Endpoint.kt
@@ -3,11 +3,14 @@ package com.senai.vsconnect.apis
 import com.google.gson.JsonObject
 import com.senai.vsconnect.models.Login
 import com.senai.vsconnect.models.Servico
+import okhttp3.MultipartBody
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.Multipart
 import retrofit2.http.POST
 import retrofit2.http.PUT
+import retrofit2.http.Part
 import retrofit2.http.Path
 import java.util.UUID
 
@@ -17,13 +20,16 @@ interface Endpoint {
     @GET("servicos")
     fun listarServicos(): Call<List<Servico>>
 
-    @PUT("usuarios/{idUsuario}")
-    fun atualizarImagemDePerfil(
-        @Path(value = "idUsuario", encoded = true) de: UUID
+    @Multipart
+    @PUT("usuarios/editarImagem/{idUsuario}")
+    fun editarImagem(
+        @Part imagem: MultipartBody.Part,
+        @Path(value = "idUsuario", encoded = true) idUsuario: UUID
     ): Call<JsonObject>
 
     @GET("usuarios/{idUsuario}")
-    fun buscarUsuarioPorID(@Path(value = "idUsuario", encoded = true) de: UUID
+    fun buscarUsuarioPorID(
+        @Path(value = "idUsuario", encoded = true) de: UUID
     ): Call<JsonObject>
 
     @POST("login")
